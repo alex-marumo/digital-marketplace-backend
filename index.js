af7282f8c4783daf24cb2c0ba8615c6f30dc6183
alex-marumo <alexmarumo16@gmail.com>
2025-03-05 17:32:49 +0200

    Added tables for reviews and messages to database schema

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const path = require('path');
 const { Pool } = require('pg');
 const cors = require('cors');
-//testing
+
 const app = express();
 const port = process.env.PORT || 3000;
 
